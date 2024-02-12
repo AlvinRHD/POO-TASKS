1beac3901373a9216203e40f62c5678bcfb1db91
AlvinRHD <ezequielhernandes907@gmail.com>
2024-02-11 20:34:11 -0600

    Agg ejercicios

---
+++ Laboratorio1/Ejercicio - 1 - Cajero Automatico/CajeroAutomaticoDemo/CajeroAutomatico/Program.cs
@@ -0,0 +1,105 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        //la contra del usuario
+        string contra = "1234";
+
+        //dinero inicial
+        decimal saldo = 1000;
+
+        Console.WriteLine("Bienvenido al ATM");
+
+        //solicitar la contra
+        Console.WriteLine("Porfavor ingrese su contraseña");
+        string contraIngreso = Console.ReadLine();
+
+        //verificar si la contra es valida
+        while (contraIngreso != contra)
+        {
+            Console.WriteLine("Contraseña incorrecta, intente nuevamente porfavor");
+
+            //solicitar denuevo la contra
+            Console.WriteLine("Porfavor ingrese su contraseña");
+            contraIngreso = Console.ReadLine();
+        }
+
+        Console.WriteLine("Contraseña correcta");
+
+        //demas funcionalidades   //menu
+        while (true)
+        {
+            Console.WriteLine(".::::::::MENU::::::::.");
+            Console.WriteLine("                      ");
+            Console.WriteLine("1.-Ver saldo ");
+            Console.WriteLine("2.-Depositar ");
+            Console.WriteLine("3.-Retiro en efectivo ");
+            Console.WriteLine("4.-Salir..... ");
+            Console.WriteLine("                      ");
+            Console.WriteLine(".::::::::MENU::::::::.");
+
+            //solicitar al usuario que quiere hacer
+            Console.Write("Porfavor elija una opcion entre el 1 y 4 ");
+            string opcion = Console.ReadLine();
+
+
+            //que procese lo que se escogio
+            switch (opcion)
+            {
+                case "1":
+                    //saldo
+                    Console.WriteLine($"Saldo actual: ${saldo}");
+                    break;
+
+                case "2":
+                    //deposito
+                    Console.WriteLine("Ingrese la cantidad a depositar: ");
+                    if (decimal.TryParse(Console.ReadLine(), out decimal cantidadDeposito))
+                    {
+                        saldo += cantidadDeposito;
+                        Console.WriteLine($"Deposito con exito, nuevo saldo: ${saldo}");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("La entrada no es valida, porfavor ingrese un monto que sea valido.");
+                    }
+                    break;
+
+                case "3":
+                    //retirar efectivo
+                    Console.WriteLine("Ingrese la cantidad a retirar: ");
+                    if (decimal.TryParse(Console.ReadLine(), out decimal cantidadRetiro))
+                    {
+                        if (cantidadRetiro <= saldo)
+                        {
+                            saldo -= cantidadRetiro;
+                            Console.WriteLine($"Retiro exitoso, nuevo saldo: ${saldo}");
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("No hay suficiente saldo para realizar el retiro.");
+                        }
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Entrada no valida porfavor ingrese un monto valido.");
+                    }
+                    break;
+
+                case "4":
+                    //salir 
+                    Console.WriteLine("Gracias por usar nuestro ATM, nos vemos luego!!!!!!!");
+                    Console.WriteLine("Presione cualquier letra para salir");
+                    Console.ReadKey();
+                    return;
+
+                default:
+                    Console.WriteLine("Opcion no valida, porfavor elija una opcion del 1 al 4");
+                    break;
+            }
+        }
+    }
+}
---
+++ Laboratorio1/Ejercicio - 2 - Simulador Musical/SimuladorMusicalDemo/SimuladorMusical/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        // las instancias
+        Instrumento guitarra = new Guitarra("Electrica");
+        Instrumento piano = new Piano("de Cola");
+        Instrumento violin = new Violin("Clasico");
+
+        //menu
+        while (true)
+        {
+            Console.WriteLine("======= SIMULADOR MUSICAL =======");
+            Console.WriteLine("                                 ");
+            Console.WriteLine("1.-Tocar la Guitarra");
+            Console.WriteLine("2.-Tocar el Piano");
+            Console.WriteLine("3.-Tocar el Violín");
+            Console.WriteLine("4.-Salir");
+            Console.WriteLine("                                 ");
+            Console.WriteLine("======= SIMULADOR MUSICAL =======");
+            Console.Write("Elija una opción (1-4): ");
+
+            if (int.TryParse(Console.ReadLine(), out int opcion))
+            {
+                switch (opcion)
+                {
+                    case 1:
+                        TocarInstrumento(guitarra);
+                        break;
+
+                    case 2:
+                        TocarInstrumento(piano);
+                        break;
+
+                    case 3:
+                        TocarInstrumento(violin);
+                        break;
+
+                    case 4:
+                        Console.WriteLine("¡Gracias por usar mi simulador musical ! Hasta luego.");
+                        return;
+
+                    default:
+                        Console.WriteLine("Opcion no valida, please elija una opcion del 1 al 4");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Entrada no valida, porfavor escoja un numero.");
+            }
+
+            Console.WriteLine(); //separador
+        }
+    }
+
+    private static void TocarInstrumento(Instrumento instrumento)
+    {
+        Console.WriteLine($"Tocando el instrumento: {instrumento.Nombre}");
+        instrumento.Sonar();
+        Console.WriteLine();
+    }
+}
+
+//la clase principal
+class Instrumento
+{
+    //propiedad instrumento
+    public string Nombre { get; }
+
+    //construcoro
+    public Instrumento(string nombre)
+    {
+        Nombre = nombre;
+    }
+
+    //metodo para sonar
+    public virtual void Sonar()
+    {
+        Console.WriteLine("¡Haciendo sonar el instrumento!");
+    }
+}
+
+//clase derivada - Guitarra
+class Guitarra : Instrumento
+{
+    //ropiedad para el tipo de guitarra
+    public string Tipo { get; }
+
+    //constructor
+    public Guitarra(string tipo) : base("Guitarra")
+    {
+        Tipo = tipo;
+    }
+
+    public override void Sonar()
+    {
+        Console.WriteLine($"Tocando la guitarra {Tipo}: ¡Tararara!");
+    }
+}
+
+//clase derivada - Piano
+class Piano : Instrumento
+{
+    public string Tipo { get; }
+
+    public Piano(string tipo) : base("Piano")
+    {
+        Tipo = tipo;
+    }
+
+    public override void Sonar()
+    {
+        Console.WriteLine($"Tocando el piano {Tipo}: ¡Plin plin plin!");
+    }
+}
+
+//clase derivada - Violín
+class Violin : Instrumento
+{
+    //propiedad para el tipo de violín
+    public string Tipo { get; }
+
+    public Violin(string tipo) : base("Violín")
+    {
+        Tipo = tipo;
+    }
+
+    public override void Sonar()
+    {
+        Console.WriteLine($"Tocando el violín {Tipo}: ¡Screeeech!");
+    }
+}
